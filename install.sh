55e35eca9ab60e9a45b0ff6cbb0f8cd434038ede
vvaks <vvaks@hortonworks.com>
2016-10-28 08:37:09 -0400

    update

--- install.sh
+++ install.sh
@@ -181,6 +181,9 @@ installNifiService () {
        	echo "*********************************Installing NIFI Service"
        	# Install NIFI Service
        	TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Install Nifi"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "INSTALLED"}}}' http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI | grep "id" | grep -Po '([0-9]+)')
+       	until ! [ -z $TASKID ]; do
+       		TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Install Nifi"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "INSTALLED"}}}'
+       	done
        	echo "*********************************AMBARI TaskID " $TASKID
        	sleep 2
        	LOOPESCAPE="false"
